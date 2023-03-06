7fba04c7f160fac0b48c8345f017bd44fcdd9ea0
sh3hryarkhan <24100181@lums.edu.pk>
2023-03-06 12:51:04 +0500

    Update WelcomeScreen.js
    Added button changes

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -6,10 +6,14 @@ function WelcomeScreen(props) {
         <View 
             style = {styles.background}
         >
-            <Image style= {styles.logo} source = {require("../assets/CampusConnect_Default.png")} />
-            <Text>Placeholder TEXT!</Text>
-            <View style = {styles.loginButton}></View>
-            <View style = {styles.registerButton}></View>
+            <Image style = {styles.logo} source = {require("../assets/CampusConnect_Default.png")} />
+            <Text style = {styles.welcomeText}>Placeholder TEXT!</Text>
+            <View style = {styles.loginButton}>
+                <Text>Login</Text>
+            </View>
+            <View style = {styles.registerButton}>
+                <Text>Register</Text>
+            </View>
 
         </View>
     );
@@ -19,28 +23,41 @@ const styles = StyleSheet.create({
     background : {
         flex : 1,
         backgroundColor : '#ECDD7B',
-        justifyContent : 'flex-end',
+        justifyContent : 'center',
         alignItems : 'center',
     },
 
     loginButton : {
-        width : '100%',
+        width : '80%',
         height : 70,
+        position : 'absolute',
+        top : 600,
+        borderRadius : 15,
         backgroundColor : 'lightblue',
     },
 
     logo : {
-        width : 200, 
+        width : 300, 
         height : 200,
         position : 'absolute',
         top : 50,
     },
     
     registerButton : {
-        width : '100%',
+        width : '80%',
         height : 70,
+        position : 'absolute',
+        top : 700,
+        borderRadius : 15,
         backgroundColor : 'dodgerblue',
+        
     },
+
+    welcomeText : {
+        position : 'absolute',
+        top : 400
+    }
+
 })
 
 export default WelcomeScreen;
