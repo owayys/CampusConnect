dc18ed2434c3d4d44158d236b291bc692149da1d
AsherJ07 <asherjavaid07@gmail.com>
2023-04-05 15:33:35 +0500

    Update Signup.js

--- screens/Signup.js
+++ screens/Signup.js
@@ -27,12 +27,11 @@ const Signup = () => {
       return;
     }
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('http://localhost:3000/api/auth/signup', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name,email,password})
+        headers: {'Content-Type': 'application/json' },
+        body: JSON.stringify({name,email,password})
       });
-
       const data = await response.json();
       if (data.code===200){
         console.log(data);
