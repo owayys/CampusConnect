f4fabcaeb7d63b7e752e85297fc63e5ae996c762
AsherJ07 <asherjavaid07@gmail.com>
2023-04-05 16:38:00 +0500

    completed heatmap+added navigation for passing name to and from map

--- screens/Signup.js
+++ screens/Signup.js
@@ -27,7 +27,7 @@ const Signup = () => {
       return;
     }
     try {
-      const response = await fetch('http://localhost:3000/api/auth/signup', {
+      const response = await fetch('http://192.168.100.15:3000/api/auth/signup', {
         method: 'POST',
         headers: {'Content-Type': 'application/json' },
         body: JSON.stringify({name,email,password})
