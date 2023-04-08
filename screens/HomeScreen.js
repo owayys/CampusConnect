863d6201c3bb7ebfa95fc0d513bbc2e8a0457d7c
AsherJ07 <asherjavaid07@gmail.com>
2023-04-09 04:42:37 +0500

    join requests screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -57,7 +57,7 @@ const HomeScreen = ({route}) => {
       <TouchableOpacity
         style={styles.rectangleGroup}
         activeOpacity={0.2}
-        onPress={() => navigation.navigate("SGJoinReq", { name: name})}
+        onPress={() => navigation.navigate("InteractiveMap", { name: name})}
       >
         <Image
           style={[styles.rectangleIcon, styles.rectangleLayout]}
